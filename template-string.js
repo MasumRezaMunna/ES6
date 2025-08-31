6e1f38595b7c012b2001acab2c586426728ddee3
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-08-31 19:31:11 +0600

    template string, multiple line string, dynamic string ()

---
+++ template-string.js
@@ -0,0 +1,27 @@
+// const porem = 'amm pata jora jora
+// marbu chabuk chorbe ghora'
+
+// const poem = 'amm pata jora jora \n'+
+// 'marbu chabuk chorbe ghora \n'+
+// 'ore khoka ghure dara'
+
+// console.log(poem)
+
+// const poem = `amm pata jora jora
+// marbo chabuk chorbe ghora
+// ore babu ghure dara
+// asche amr pagla ghora`
+
+// console.log(poem)
+
+function sum (num1, num2){
+    const result = num1 + num2;
+    // const output = 'sum of' + num1 + 'and' + num2 + 'is equal to' + result;
+    const output = `sum of ${num1} and ${num2} is equal to ${result}`
+    console.log(output)
+}
+sum(10,20)
+
+const price = 100;
+const discountedPrice = `Discounted price of ${price} is after 20% discount ${price * .8}`
+console.log(discountedPrice)
