fcceff69f9653f9d10628063f88b5f8690b821b2
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-09-02 16:02:13 +0600

    loop through an object using for in

---
+++ freeze-seal.js
@@ -0,0 +1,31 @@
+// const employee = {
+//     name: 'Raja Rani',
+//     designation: 'QA',
+//     salary: 20000,
+//     experience: 1,
+//     age: 22
+// }
+// Object.freeze(employee)
+// // delete employee.experience
+
+// employee.salary = employee.salary + 5000;
+// employee.location = 'Dhaka'
+// console.log(employee)
+
+
+
+const employee = {
+    name: 'Raja Rani',
+    designation: 'QA',
+    salary: 20000,
+    experience: 1,
+    age: 22
+}
+
+Object.seal(employee)
+
+delete employee.experience
+
+employee.salary = employee.salary + 5000;
+employee.location = 'Dhaka'
+console.log(employee)
