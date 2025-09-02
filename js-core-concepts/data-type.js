945e8e9beee76588adac134e223c04be3ba2bffb
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-09-02 22:45:49 +0600

    primitive and non-primitive data type

---
+++ js-core-concepts/data-type.js
@@ -0,0 +1,26 @@
+// js is a dynamic typed programming language
+let data = 4;
+data = 'tormuj'
+data = false
+data = {name: 'vangi', price: 40}
+data = [445]
+data = undefined
+data = null
+
+
+console.log(typeof data);
+
+
+// hs: primitive data type
+const price = 50;
+const name = 'bodrul';
+const isPoor = false;
+let isKnown = undefined ;
+let isLocal;
+const merit = null;
+
+console.log(isKnown, isLocal)
+
+// non primitive data type
+const student = { name: 'badal', location: 'bristi goli', subject: 'botany'};
+const friends = ['tom', 'bob', 'jack', 'rock']
