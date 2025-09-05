0956b0bf36179d803d2d77ca0a438379c4870093
MasumRezaMunna <masumrezamunna2005@gmail.com>
2025-09-05 11:41:24 +0600

    callback function and pass different functions

---
+++ js-core-concepts/callback.js
@@ -0,0 +1,27 @@
+// callback
+function pakhiBhai (callMeBack, patro, patri){
+
+    console.log('value of patro', patro)
+    console.log('callMeBack parameter', callMeBack)
+
+    if(patri){
+        // console.log(callMeBack)
+        callMeBack(patro)
+    }
+    else{
+        console.log('tor kopale biya nai')
+    }
+}
+
+function callSomeOne (person){
+console.log('calling', person)
+}
+
+// callSomeOne('jodu')
+// pakhiBhai(callSomeOne, 'jodu', 'modu')
+// muBtn.addEventListener('click', () => {
+
+// })
+const addFive = x => x + 5
+
+[1,2,3,5,5].map(addFive)
